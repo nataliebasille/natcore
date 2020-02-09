e7bf0caa26b477f28f90f0199d33c134a56097a3
Natalie <natalie.coleman39@gmail.com>
2020-02-09 10:06:55 -0500

    Add value converter to store enums as strings in a database

---
+++ _tests/Natcore_Entityframework/Entities.cs
@@ -0,0 +1,20 @@
+﻿namespace Natcore_Entityframework
+{
+    public class TestEntity
+    {
+        public int Key { get; set; }
+
+        public int IntProp { get; set; }
+
+        public string StringProp { get; set; }
+
+        public TestEnum EnumProp { get; set; }
+    }
+
+    public enum TestEnum
+    {
+        One,
+        Two,
+        Three
+    }
+}
---
+++ _tests/Natcore_Entityframework/TestContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Natcore.EntityFramework.Converters;
+using System;
+
+namespace Natcore_Entityframework
+{
+    public class TestContext : DbContext, IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        private TestContext(SqliteConnection connection, DbContextOptions<TestContext> options)
+            : base(options) 
+        {
+            _connection = connection;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var testEntityBuilder = modelBuilder.Entity<TestEntity>();
+            testEntityBuilder.HasKey(x => x.Key);
+
+            testEntityBuilder.Property(x => x.EnumProp)
+                .HasConversion(ValueConverters.EnumToString<TestEnum>());
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            if (_connection.State == System.Data.ConnectionState.Open)
+                _connection.Close();
+
+            _connection.Dispose();
+        }
+
+        public static TestContext CreateContext()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            var context = new TestContext(connection, options);
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+}
