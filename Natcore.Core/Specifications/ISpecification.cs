481b794136711ddea4eaed1bed705b87f42e2f0b
Natalie <natalie.coleman39@gmail.com>
2020-04-25 18:31:41 -0400

    Add queryable specifications

---
+++ Natcore.Core/Specifications/ISpecification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Natcore.Core.Specifications
+{
+    public interface ISpecification<in TIn, out TOut>
+    {
+        IQueryable<TOut> Build(IQueryable<TIn> query);
+    }
+
+    public interface ISpecification<T> : ISpecification<T, T> { }
+}
