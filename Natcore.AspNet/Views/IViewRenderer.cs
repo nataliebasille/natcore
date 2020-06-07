a6187aff779dcb785aeb0707548bff53075cd274
Natalie <natalie.coleman39@gmail.com>
2020-06-07 15:24:08 -0400

    Add view engine renderer

---
+++ Natcore.AspNet/Views/IViewRenderer.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Natcore.AspNet.Views
+{
+    public interface IViewRenderer
+    {
+        Task<string> RenderAsync<TModel>(string viewName, TModel model);
+    }
+}
---
+++ Natcore.AspNet/Views/ViewRendererExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+namespace Natcore.AspNet.Views
+{
+    public static class ViewRendererExtensions
+    {
+        public static IServiceCollection AddViewRenderer(this IServiceCollection services)
+        {
+            services
+                .AddScoped<IViewRenderer, RazorViewRenderer>()
+                .AddMvc()
+                .AddRazorOptions(o =>
+                {
+                    o.ViewLocationFormats.Add("/Emails/{0}" + RazorViewEngine.ViewExtension);
+                });
+
+            return services;
+        }
+    }
+}
